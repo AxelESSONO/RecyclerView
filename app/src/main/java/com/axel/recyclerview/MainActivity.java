0827b383d63a0899127d8c0dce3d957a8d289961
axel-essono <naxelcdric@gmail.com>
2023-06-16 10:13:26 +0200

    recyclerview (suite)

--- app/src/main/java/com/axel/recyclerview/MainActivity.java
+++ app/src/main/java/com/axel/recyclerview/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private List<Felin> felins;
+    private List<Felin> felinList;
     private FelinAdapter felinAdapter;
 
     @Override
@@ -22,35 +22,58 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.felinRecycler);
 
-
+        loadFelinData();
+        setRecyclerView();
 
     }
 
 
 
-
-
-
-
-
-
-
-
-
-
+    // Chargement de la liste : felinList
     private void loadFelinData() {
-        felins.add(new Felin("Chat", R.drawable.chat));
-        felins.add(new Felin("Jaguard", R.drawable.jaguard));
-        felins.add(new Felin("Lion", R.drawable.lion));
-        felins.add(new Felin("Puma", R.drawable.puma));
-        felins.add(new Felin("Serval", R.drawable.servalm));
-        felins.add(new Felin("Tigre", R.drawable.tigre));
+
+        felinList = new ArrayList<>();
+
+        felinList.add(
+                new Felin("Chat", R.drawable.chat)
+        );
+
+        felinList.add(
+                new Felin("Jaguard", R.drawable.jaguard)
+        );
+        felinList.add(
+                new Felin("Lion", R.drawable.lion)
+        );
+        felinList.add(
+                new Felin("Puma", R.drawable.puma)
+        );
+        felinList.add(
+                new Felin("Serval", R.drawable.servalm)
+        );
+        felinList.add(
+                new Felin("Tigre", R.drawable.tigre)
+        );
     }
 
-    private void setRecyclerView() {
+  /*  private void setRecyclerView() {
         felinAdapter = new FelinAdapter(felins);
         recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
         recyclerView.setAdapter(felinAdapter);
+        recyclerView.setHasFixedSize(true);
+    }*/
+
+    private void setRecyclerView() {
+        felinAdapter = new FelinAdapter(felinList);
+
+        // lier FelinAdapter au RecyclerView
+        recyclerView.setAdapter(felinAdapter);
+        // Disposition des items du RecyclerView
+        recyclerView.setLayoutManager(
+                new GridLayoutManager(
+                        MainActivity.this,
+                        2
+                )
+        );
         recyclerView.setHasFixedSize(true);
     }
 
